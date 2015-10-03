383af418f6acad0745b32881b6b7644021ce9034
Johannes Klumpe <klumpe@live.de>
2015-10-03 04:15:20 +0200

    Minor changes

--- gulp/lint.js
+++ gulp/lint.js
@@ -9,17 +9,32 @@ var reload = browserSync.reload;
 function lint(files, options) {
   return function() {
     return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
+      .pipe(reload({
+        stream: true,
+        once: true
+      }))
       .pipe($.eslint(options))
       .pipe($.eslint.format())
       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
   };
 }
-var testLintOptions = {
-  env: {
-    mocha: true
-  }
+
+var lintOptions = {
+    "rules": {
+      "camelcase": 1,
+      "comma-dangle": 2,
+      "quotes": 0
+    },
+    "globals": {
+      "jQuery": false,
+      "$": true
+    }
+  },
+  testLintOptions = lintOptions;
+
+testLintOptions.env = {
+  mocha: true
 };
 
-gulp.task('lint', lint('app/scripts/**/*.js'));
+gulp.task('lint', lint('app/scripts/**/*.js', lintOptions));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
